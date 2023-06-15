7e7807fb91768cd9b8135e99ea2a9e22244f0225
hezekiah02 <olahex@gmail.com>
2023-06-15 12:51:16 +0100

    Update CheckoutForm.js

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-lyart.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `https://jupa-o1ez6ba3x-hezekiah02.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
